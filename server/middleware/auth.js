27aeb0a75d1ab7b82dcf76a7f98c47418e39b478
Zergma <104103418+Zergma@users.noreply.github.com>
2026-05-04 18:15:20 -0400

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,12 +1,18 @@
-function requireAuth(req, res, next) {
+function requireAuthAPI(req, res, next) {
     if (!req.session.userId) {
         return res.status(401).json({
             success: false,
             message: "Unauthorized"
         });
     }
-
     next();
 }
 
-module.exports = requireAuth;
+function requireAuthPage(req, res, next) {
+    if (!req.session.userId) {
+        return res.redirect("/login.html");
+    }
+    next();
+}
+
+module.exports = { requireAuthAPI, requireAuthPage };
