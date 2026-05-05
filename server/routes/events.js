fcb596666010f096c4b5a45900f3434487227120
Zergma <104103418+Zergma@users.noreply.github.com>
2026-05-05 15:19:20 -0400

    Update events.js

--- server/routes/events.js
+++ server/routes/events.js
@@ -5,12 +5,13 @@ const { requireAuthAPI } = require("../middleware/auth");
 module.exports = (db) => {
 
 // Create event
-router.post("/", requireAuthAPI, (req, res) => {
+router.post("/", requireAuthAPI, async (req, res) => {
     const { title, description, start_time, end_time, visibility, recurrence, color } = req.body;
 
     db.query(
-        "INSERT INTO events (user_id, title, description, start_time, end_time, visibility, recurrence, color) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
-        [req.session.userId, title, description, start_time, end_time, visibility, recurrence, color],
+        `INSERT INTO events (user_id, title, description, start_time, end_time, visibility, recurrence, color)
+        VALUES ($1,$2,$3,$4,$5,$6,$7,$8)`,
+        [req.user.id, title, description, start_time, end_time, visibility, recurrence, color],
         (err) => {
             if (err) {
                 console.error(err);
@@ -22,15 +23,17 @@ router.post("/", requireAuthAPI, (req, res) => {
 });
 
 // Get my events
-router.get("/", requireAuthAPI, (req, res) => {
-    db.query(
-        "SELECT * FROM events WHERE user_id = ?",
-        [req.session.userId],
-        (err, results) => {
-            if (err) return res.status(500).json({ success: false });
-            res.json(results);
-        }
-    );
+router.get("/", requireAuthAPI, async (req, res) => {
+    try {
+        const result = await db.query(
+            "SELECT * FROM events WHERE user_id = $1",
+            [req.user.id]
+        );
+
+        res.json(result.rows);
+    } catch (err) {
+        res.status(500).json({success: false });
+    }
 });
 /*
 // Backend route for dragging and dropping events
@@ -39,7 +42,7 @@ router.put("/:id", requireAuthAPI, (req, res) => {
 
     db.query(
         "UPDATE events SET title = ?, start_time = ? WHERE id = ? AND user_id = ?",
-        [title, start_time, req.params.id, req.session.userId],
+        [title, start_time, req.params.id, req.user.id],
         (err) => {
             if (err) return res.status(500).json({ success: false });
             res.json({ success: true });
@@ -48,15 +51,17 @@ router.put("/:id", requireAuthAPI, (req, res) => {
 });*/
 
 // Backend routes for edit/delete
-router.delete("/:id", requireAuthAPI, (req, res) => {
-    db.query(
-        "DELETE FROM events WHERE id = ? AND user_id = ?",
-        [req.params.id, req.session.userId],
-        (err) => {
-            if (err) return res.status(500).json({ success: false });
-            res.json({ success: true });
-        }
-    );
+router.delete("/:id", requireAuthAPI, async (req, res) => {
+    try {
+        await db.query(
+            "DELETE FROM events WHERE id = $1 AND user_id = $2",
+            [req.params.id, req.user.id]
+        );
+
+        res.json({ success: true })
+    } catch (err){
+        res.status(500).json({ success: false });
+    }
 });
 
 return router;
