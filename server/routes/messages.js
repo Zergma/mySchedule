da9f7ba6f47ba66cfc4390a898123a17e425c40b
Zergma <104103418+Zergma@users.noreply.github.com>
2026-05-05 15:19:29 -0400

    Update messages.js

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -8,10 +8,10 @@ module.exports = (db) => {
         const { receiverId, message } = req.body;
 
         db.query(
-            "INSERT INTO messages (sender_id, receiver_id, message) VALUES (?, ?, ?)",
-            [req.session.userId, receiverId, message],
+            "INSERT INTO messages (sender_id, receiver_id, message) VALUES ($1, $2, $3)",
+            [req.user.id, receiverId, message],
             (err) => {
-                /*console.log("Sending message:", req.session.userId, receiverId, message);
+                /*console.log("Sending message:", req.user.id, receiverId, message);
                 console.error(err);*/
                 if (err) return res.status(500).json({ success: false });
                 res.json({ success: true });
@@ -21,14 +21,14 @@ module.exports = (db) => {
 
     router.get("/:userId", requireAuthAPI, (req, res) => {
         const otherUserId = req.params.userId;
-        const currentUserId = req.session.userId;
+        const currentUserId = req.user.id;
 
         db.query(
             `SELECT * FROM messages
-            WHERE (sender_id = ? AND receiver_id = ?)
-                OR (sender_id = ? AND receiver_id = ?)
+            WHERE (sender_id = $1 AND receiver_id = $2)
+                OR (sender_id = $2 AND receiver_id = $1)
             ORDER BY created_at ASC`,
-            [currentUserId, otherUserId, otherUserId, currentUserId],
+            [currentUserId, otherUserId],
             (err, results) => {
                 if (err) return res.status(500).json({ success: false });
                 res.json(results);
